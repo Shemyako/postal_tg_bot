ad068072053fd08d80a7d99ad28e89d299a6c0b3
Shemyako <52855609+Shemyako@users.noreply.github.com>
2023-02-24 21:47:28 +0300

    is_active mails checking. mail deleting added

--- bot/handlers/work_with_mail.py
+++ bot/handlers/work_with_mail.py
@@ -3,7 +3,7 @@ from aiogram.types import ReplyKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 import db
 import uuid
-from sqlalchemy import insert, select
+from sqlalchemy import insert, select, update, and_
 from bot import config
 
 
@@ -15,7 +15,11 @@ router = Router()  # [1]
 
 @router.message(lambda message: message.from_user.id in config.whitelist and message.text=="Добавить почту")
 async def mail_creation(message: types.Message):
-    query = select([db.customers.c.status, db.mails.c.owner]).join(db.customers).where(db.mails.c.owner == message.from_user.id)
+    query = select([db.customers.c.status, db.mails.c.owner]).join(db.customers).where(
+        and_(db.mails.c.owner == message.from_user.id,
+            db.mails.c.is_active == True
+        )
+    )
     mails =  await db.database.fetch_all(query)
 
     kb = [[types.KeyboardButton(text="Меню")]]
@@ -23,21 +27,23 @@ async def mail_creation(message: types.Message):
 
     # Если уже есть почты и статус обычный,
     # то больше почту создать не дадим
-    if mails != [] and mails[0][0] == 0:
-        await message.answer("У Вас уже есть одна почта.", reply_markup=keyboard)
-        return
+    if mails and mails[0][0] == 0:
+        return await message.answer("У Вас уже есть одна почта.", reply_markup=keyboard)
 
     mails = uuid.uuid4()
     query = insert(db.mails)
     result = await db.database.execute(query, {"name":f"{mails}", "owner":message.from_user.id})
-
 
     await message.answer(f"`{mails}` \- Ваша почта", parse_mode="MarkdownV2", reply_markup=keyboard)
     
 
 @router.message(lambda message: message.from_user.id in config.whitelist and message.text=="Список почт")
 async def mail_showing(message: types.Message):
-    query = select([db.mails.c.name]).where(db.mails.c.owner == message.from_user.id)
+    query = select([db.mails.c.name]).where(
+        and_(db.mails.c.owner == message.from_user.id,
+            db.mails.c.is_active == True  
+        )
+    )
 
     mails =  await db.database.fetch_all(query)
     mails = "\n".join(["`" + i[0] + "`" for i in mails])
@@ -46,27 +52,31 @@ async def mail_showing(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True, one_time_keyboard=True)
     
     # Если почт нет
-    if mails == []:
-        await message.answer("Список почт пуст.", reply_markup=keyboard)
-        return
+    if not mails:
+        return await message.answer("Список почт пуст.", reply_markup=keyboard)
     else:
         await message.answer(f"{mails}\nВаши почты", parse_mode="MarkdownV2", reply_markup=keyboard)
 
 
 @router.message(lambda message: message.from_user.id in config.whitelist and message.text=="Удалить почту")
 async def pre_mail_deleting(message: types.Message):
-    query = select([db.mails.c.name]).where(db.mails.c.owner == message.from_user.id)
-
+    query = select([db.mails.c.name]).where(
+        and_(
+            db.mails.c.owner == message.from_user.id,
+            db.mails.c.is_active == True
+        )
+    )
     mails =  await db.database.fetch_all(query)
 
-    if mails == []:
-        await message.answer("У вас нет почт.")
-        return
+    if not mails:
+        kb = [[types.KeyboardButton(text="Меню")]]
+        keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True, one_time_keyboard=True)
+        return await message.answer("У вас нет почт.", reply_markup=keyboard)
 
     
     builder = InlineKeyboardBuilder()
     for i in mails:
-        print(i)
+        # print(i)
         builder.row(types.InlineKeyboardButton(
             text=i[0], callback_data=f"delete {i[0]}")
         )
@@ -78,6 +88,26 @@ async def pre_mail_deleting(message: types.Message):
     await message.answer("Нажмите почту для удаления.", reply_markup=builder.as_markup())
 
 
-@router.callback_query(lambda call: len(call.data)>7 and  call.data[:6]=="delete")
+@router.callback_query(lambda call: call.from_user.id in config.whitelist and len(call.data)>7 and  call.data[:6]=="delete")
 async def mail_deleting(call: types.CallbackQuery):
-    print(call)
+    query = select([db.mails.c.id]).where(
+        and_(
+            db.mails.c.owner == call.from_user.id, 
+            db.mails.c.name == call.data[7], 
+            db.mails.c.is_active == True
+        )
+    )
+    mails =  await db.database.fetch_one(query)
+
+    if mails:
+        print(mails['id'])
+        query = update(db.mails).where(mails['id'] == db.mails.c.id).values(is_active = False)
+        print(query)
+        await db.database.execute(query)
+    else:
+        
+        # Добавить всплывающее окно
+        pass
+
+    await call.message.delete()
+    await pre_mail_deleting(call.message)
